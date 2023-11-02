66aca973b2faa8940f8fbc45606769d0113e6c27
Maverick Vitto <mavvitto@yahoo.com>
2023-11-02 15:24:28 -0600

    java functions continued

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`
+}
+
+function mondayWork (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (flare = '*') {
+    return function(adjective = "special") {
+        return `You are ${flare}${adjective}${flare}!`
+    } 
+}
+
+
